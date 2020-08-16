3edf083fb4288080f8a0b0ff1e2988262e00a119
alfredotp <dalfredot@gmail.com>
2020-08-16 04:45:48 -0500

    tercer commit

---
+++ reto_funciones.py
@@ -0,0 +1,19 @@
+# Crear una función que imprima un mensaje de bienvenida
+def mensaje_bienvenida():
+     print('Bienvenido al curso de Python')
+
+mensaje_bienvenida()
+
+# Crear una función que tome un nombre de un usuario y lo muestre como mensaje de bienvenida
+usuario = 'alfredo'
+
+def nombre(usuario):
+     print(f'Bienvenido al curso de Python {usuario}')
+
+nombre(usuario)
+
+# Crear una función que tome la última actividad que hiciste
+def actividad():
+     return nombre(usuario)
+
+actividad()
